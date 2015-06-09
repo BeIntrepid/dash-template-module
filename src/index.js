68eb65ae0338e28922f70a756d0d3355daede953
Az <azim.palmer@googlemail.com>
2015-06-09 13:45:14 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+// This is the file that's run when referenced, so if you want to expose something import it then export it here
+
+//import {serviceLocator} from './serviceLocator'
+//export {serviceLocator} from './serviceLocator'
